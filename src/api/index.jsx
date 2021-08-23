6f43e27ee99413cce063675ffa2358c47281e39c
Waaiez Kinnear <waaiez.kinnear10@gmail.com>
2021-08-23 20:48:41 +0200

    Fix API and install Axios

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -3,23 +3,40 @@ import axios from 'axios';
 const twitterURL = 'https://api.waaiez.repl.co/twitter';
 const youtubeURL = 'https://api.waaiez.repl.co/youtube';
 const mlhURL = 'https://api.waaiez.repl.co/mlh';
-const quizURL = 'https://api.waaiez.repl.co/quiz'
-
+const quizURL = 'https://api.waaiez.repl.co/quiz';
 
 export const fetchTwitterData = async () => {
-    try {
-        const data = await axios.get(twitterURL);
-        return data;
-    } catch (error) {
-        return error;
-    }
+	try {
+		const data = await axios.get(twitterURL);
+		return data;
+	} catch (error) {
+		return error;
+	}
 };
-export const fetchYoutubeData = async () => {
-    try {
-        const data = await axios.get(youtubeURL);
-        return data;
 
-    } catch (error) {
-        return error;
-    }
+export const fetchYoutubeData = async () => {
+	try {
+		const data = await axios.get(youtubeURL);
+		return data;
+	} catch (error) {
+		return error;
+	}
+};
+
+export const fetchMlhData = async () => {
+	try {
+		const data = await axios.get(mlhURL);
+		return data;
+	} catch (error) {
+		return error;
+	}
+};
+
+export const fetchQuizData = async (category, difficulty) => {
+	try {
+		const data = await axios.get(`${quizURL}/${category}/${difficulty}/10`);
+		return data;
+	} catch (error) {
+		return error;
+	}
 };
