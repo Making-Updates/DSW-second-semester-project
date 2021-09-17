bbdc9f68e0fc33226426f5b8b7593f36948b3763
Waaiez Kinnear <waaiez.kinnear10@gmail.com>
2021-09-17 17:12:46 +0200

    Begin styling Settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,30 +1,94 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+	IonButtons,
+	IonContent,
+	IonHeader,
+	IonMenuButton,
+	IonPage,
+	IonTitle,
+	IonToolbar,
+	IonListHeader,
+	IonNote,
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 
+// delete
+import { useHistory } from 'react-router-dom';
+import { supabase } from '../supabase';
+// delete
+
 const Page = () => {
+	// delete
+	const history = useHistory();
+	function login() {
+		history.push('/page/Login');
+	}
+	function register() {
+		history.push('/page/Register');
+	}
+	function logout() {
+		supabase.auth
+			.signOut()
+			.then((response) => {
+				history.push('/page/Login');
+			})
+			.catch((err) => {
+				alert(err.response.text);
+			});
+	}
+	// delete
 
-    return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonButtons slot="start">
-                        <IonMenuButton />
-                    </IonButtons>
-                    <IonTitle>Settings</IonTitle>
-                </IonToolbar>
-            </IonHeader>
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonButtons slot='start'>
+						<IonMenuButton />
+					</IonButtons>
+					<IonTitle>Settings</IonTitle>
+				</IonToolbar>
+			</IonHeader>
 
-            <IonContent fullscreen>
-                <IonHeader collapse="condense">
-                    <IonToolbar>
-                        <IonTitle size="large">Settings</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                {/* Insert components under here */}
-                <ExploreContainer name="Settings" />
-            </IonContent>
-        </IonPage>
-    );
+			<IonContent fullscreen>
+				<IonHeader collapse='condense'>
+					<IonToolbar>
+						<IonTitle size='large'>Settings</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+
+				<hr />
+				<div className='d-flex justify-content-center my-5'>
+					<button className='btn btn-light btn-lg me-3 w-50'>
+						Lock the app
+					</button>
+				</div>
+				<hr />
+				<div className='d-flex justify-content-center my-5'>
+					<button
+						className='btn btn-light btn-lg me-3 w-50'
+						onClick={() => {
+							history.push('/page/About');
+						}}
+					>
+						About
+					</button>
+				</div>
+				<hr />
+				<div className='d-flex justify-content-center my-5'>
+					<button
+						className='btn btn-light btn-lg me-3 w-50'
+						onClick={logout}
+					>
+						Sign Out
+					</button>
+				</div>
+				<hr />
+				{/* delete */}
+				<button onClick={login}>login</button>
+				<button onClick={register}>register</button>
+				{/* delete */}
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default Page;
