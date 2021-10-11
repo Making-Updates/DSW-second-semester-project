30988d81a3d49f6e4d7a36ca461878b7f3109e38
Waaiez Kinnear <waaiez.kinnear10@gmail.com>
2021-10-11 13:37:33 +0200

    Change Class to ClassName, add key to tr

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -1,80 +1,26 @@
-import React from 'react'
+const Leaderboard = ({ data }) => {
+	return (
+		<div>
+			<table className='table table-hover'>
+				<thead>
+					<tr>
+						<th scope='col'>#</th>
+						<th scope='col'>Name</th>
+						<th scope='col'>Score</th>
+					</tr>
+				</thead>
+				<tbody>
+					{data.map((row, index) => (
+						<tr key={index}>
+							<th scope='row'>{index + 1}</th>
+							<td>{row.email}</td>
+							<td>{row.score}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
 
-const Leaderboard = ({data}) => {
-    console.log("component" + data);
-    return (
-        <div>
-            <table class="table table-hover">
-            <thead>
-                <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Score</th>
-                </tr>
-            </thead>
-            <tbody>
-            {data.map((row, index) => (
-                <tr>
-                    <th scope="row">{index + 1}</th>
-                    <td>{row.email}</td>
-                    <td>{row.score}</td>
-                </tr>
-            ))}
-            </tbody>
-            </table>
-            {/* <table>
-                    <thead>
-                        <tr>
-                            <th className="title">Player Name</th>
-                            <th className="title">Score</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        // content will be manipulated with javascript
-                        <tr>
-                            <td>Tiiso</td>
-                            <td>100</td>
-                        </tr>
-                        <tr>
-                            <td>Tiiso</td>
-                            <td>100</td>
-                        </tr>
-                        <tr>
-                            <td>Tiiso</td>
-                            <td>100</td>
-                        </tr>
-                        <tr>
-                            <td>Tiiso</td>
-                            <td>100</td>
-                        </tr>
-                        <tr>
-                            <td>Tiiso</td>
-                            <td>100</td>
-                        </tr>
-                        <tr>
-                            <td>Tiiso</td>
-                            <td>100</td>
-                        </tr>
-                        <tr>
-                            <td>Tiiso</td>
-                            <td>100</td>
-                        </tr>
-                        <tr>
-                            <td>Tiiso</td>
-                            <td>100</td>
-                        </tr>
-                        <tr>
-                            <td>Tiiso</td>
-                            <td>100</td>
-                        </tr>
-                        <tr>
-                            <td>Tiiso</td>
-                            <td>100</td>
-                        </tr>
-                    </tbody>
-                </table> */}
-        </div>
-    )
-}
-
-export default Leaderboard
+export default Leaderboard;
